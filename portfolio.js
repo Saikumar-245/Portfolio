2b0830256adca7830bc76d92ce5daca62293c865
Saik2012 <kondasaikumar2020@gmail.com>
2025-03-12 21:25:12 +0530

    initial commit

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+function Change()
+{
+    var open=document.getElementById("opendiv");
+   
+    if(open.style.display===`none` ) 
+    {
+        open.style.display="block";
+       
+    } 
+    else
+    {
+        open.style.display="block";
+       
+    }
+
+}
